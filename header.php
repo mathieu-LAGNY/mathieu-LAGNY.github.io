a286c7f43a63d2cd2283c44247f4d1468b48a1a8
mathieu-LAGNY <mathieu.lagny@etu.umontpellier.fr>
2020-03-15 13:32:08 +0100

    test php

---
+++ header.php
@@ -0,0 +1,33 @@
+<header>
+    <nav class="nav-wraper white">
+      <div class="container">
+        <a href="#" data-target="slide-out" class="brand-logo grey-text text-darken-3 sidenav-trigger">Mathieu</a>
+        <a href="#" data-target="slide-out" class="brand-logo grey-text text-darken-3 hide-on-med-and-down">Mathieu LAGNY</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a class="grey-text text-darken-3" href="#about">Informations</a></li>
+          <li><a class="grey-text text-darken-3" href="#skills">Compétences</a></li>
+          <li><a class="grey-text text-darken-3" href="#interests">Centres d'intérêt</a></li>
+          <li><a class="grey-text text-darken-3 modal-trigger" href="#contact">Contact</a></li>
+          <li><a class="waves-effect waves-light btn light-blue" href="index.en.html"><i class="material-icons right">language</i>English Version</a></li>
+        </ul>
+      </div>
+    </nav>
+    <ul id="slide-out" class="sidenav container">
+      <li>
+        <div class="user-view">
+          <div class="background">
+           <img src="images/bg.jpg" alt="background">
+          </div>
+          <a href="#user"><img alt="avatar" class="circle" src="./images/avatar.jpg"></a>
+          <a href="#name"><span class="white-text name">Mathieu LAGNY</span></a>
+          <a href="#email"><span class="white-text email">m.lagny@laposte.net</span></a>
+        </div>
+      </li>
+      <li><a class="waves-effect waves-light btn-flat" href="index.en.html">English Version</a></li>
+      <li><a class="grey-text text-darken-3 waves-effect" href="#about">Informations</a></li>
+      <li><a class="grey-text text-darken-3 waves-effect" href="#skills">Compétences</a></li>
+      <li><a class="grey-text text-darken-3 waves-effect" href="#interests">Centres d'intérêt</a></li>
+      <li><a class="btn light-blue waves-effect waves-light modal-trigger" href="#contact">Contact</a></li>
+      <li><a onclick="M.toast({html: 'CV ouvert'})" href="./doc/CV.pdf" class="waves-effect waves-light btn blue darken-4">DOWNLOAD CV</a></li>
+    </ul>
+  </header>
